f1a62ad3c6a1d2f4936525b9a462a5bbbda514c7
MicchelFAB <micchelfab@gmail.com>
2022-11-28 19:29:21 +0000

    Clean

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mamaral- <mamaral-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:52:35 by mamaral-          #+#    #+#             */
-/*   Updated: 2022/11/28 19:27:50 by mamaral-         ###   ########.fr       */
+/*   Updated: 2022/11/28 19:28:53 by mamaral-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,16 @@ int	ft_atoi(const char *nptr)
 	sinal = 1;
 	if (!nptr)
 		return (0);
-	while (*nptr == 32 || (*nptr >= 9 && *nptr <= 13))	//delimitate spaces, tabs and new lines
-		nptr++;					//increment pointer
-	if (*nptr == '-')					//check if string has a sign negative
-		sinal *= -1;					// set sign to positive
-	if (*nptr == '-' || *nptr == '+')			//check if string has a sign
-		nptr++;					//increment pointer
-	while (*nptr >= '0' && *nptr <= '9')			//check if string has a number
+	while (*nptr == 32 || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-')
+		sinal *= -1;
+	if (*nptr == '-' || *nptr == '+')
+		nptr++;
+	while (*nptr >= '0' && *nptr <= '9')
 	{
-		res = res * 10 + *nptr - '0';			//set result to the number
-		nptr++;					//increment pointer
+		res = res * 10 + *nptr - '0';
+		nptr++;
 	}
 	return (res * sinal);
 }
